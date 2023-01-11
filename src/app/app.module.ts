fd6f0fe6dfd99634e753a58fa875a20523d5b6d5
widia-a <wwiidiiaa@gmail.com>
2023-01-11 13:22:28 +0700

    generate report page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReportPageComponent } from './report-page/report-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReportPageComponent
   ],
   imports: [
     BrowserModule,
