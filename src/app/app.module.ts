5cfe1825d6c64dbda24ff3f2416a10f9e406b5a4
rikyhdyt <rikyhd015@gmail.com>
2023-01-13 09:05:30 +0700

    get data done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { UserPageComponent } from './user-page/user-page.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
@@ -12,8 +11,7 @@ import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    UserPageComponent,
-    SidebarComponent,
+    UserPageComponent
   ],
   imports: [
     BrowserModule,
