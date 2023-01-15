4f79e0e4c3627048ccbad1d68735f1ac220879ec
widia-a <wwiidiiaa@gmail.com>
2023-01-15 10:06:35 +0700

    tambahin API

---
+++ src/app/services/base.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BaseService {
+  baseURL: string = "http://localhost:4200/api/v1"
+
+  constructor() { }
+}
